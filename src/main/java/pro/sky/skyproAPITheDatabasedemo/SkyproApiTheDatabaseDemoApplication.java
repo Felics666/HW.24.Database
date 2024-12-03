5d0af050c7e38d12416318053907adf47c8b01e8
Felics666 <felics6@gmail.com>
2024-12-03 14:39:51 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproAPITheDatabasedemo/SkyproApiTheDatabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproAPITheDatabasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproApiTheDatabaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproApiTheDatabaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproAPITheDatabasedemo/SkyproApiTheDatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproAPITheDatabasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproApiTheDatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
